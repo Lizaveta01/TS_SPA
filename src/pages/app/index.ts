d2dfa058349b0cf7b607038076630b86e9d851ad
Lizaveta Petrova <goldglaider@gmail.com>
2022-06-25 02:29:24 +0300

    feat: add settings and statistics pages

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,14 +1,21 @@
+import MainPage from "../main";
+import SettingsPage from "../settings";
+import StatisticsPage from "../statistics";
+
 class App{
   private container: HTMLElement; //любой элемент, который можно получить через document.querySelector(''), например document.body
+  private initialPage: MainPage;
+
 
 
   constructor(){
     this.container = document.body;
+    this.initialPage = new MainPage('main-page');
   }
 
-
   run() {
-    this.container.innerText = 'Single Page Application' // добавление текста на document.body
+  const mainPageHTML = this.initialPage.render();
+  this.container.append(mainPageHTML)
   }
 }
 
