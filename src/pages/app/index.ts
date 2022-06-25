7d3899cc61a7ab5672268d27b09c34f2a5f49801
Lizaveta Petrova <goldglaider@gmail.com>
2022-06-25 03:13:14 +0300

    feat: add settings and statistics pages in class App

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,21 +1,39 @@
 import MainPage from "../main";
 import SettingsPage from "../settings";
 import StatisticsPage from "../statistics";
+import Page from "../../core/templates/page";
 
 class App{
-  private container: HTMLElement; //любой элемент, который можно получить через document.querySelector(''), например document.body
+  private container: HTMLElement; 
   private initialPage: MainPage;
 
+  static renderNewPage(idPage:string) {
+    document.body.innerHTML = '';
+    let page: Page | null = null;
 
+    if (idPage === 'main-page'){
+      page = new MainPage(idPage)
+    } else if ( idPage === 'settings-page'){
+      page = new SettingsPage(idPage)
+    } else if (idPage === 'statistics-page'){
+      page = new StatisticsPage(idPage)
+    }   
+
+    if (page) {
+      const pageHTML = page.render();
+      document.body.append(pageHTML);
+    }
+    
+  }
 
   constructor(){
     this.container = document.body;
     this.initialPage = new MainPage('main-page');
   }
+  
 
   run() {
-  const mainPageHTML = this.initialPage.render();
-  this.container.append(mainPageHTML)
+    App.renderNewPage('statistics-page')
   }
 }
 
